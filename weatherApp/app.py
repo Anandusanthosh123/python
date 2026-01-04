2ebece5db2e6d29b3208d9b5be13596227379255
Anandusanthosh123 <anandusanthosh1official@gmail.com>
2026-01-04 17:21:18 +0530

    Weather App that fetch realtime weather using python

---
+++ weatherApp/app.py
@@ -0,0 +1,24 @@
+import requests
+
+API_KEY = "174fdfffae9e6e460b865073fa17d354"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+
+def get_weather(city):
+    params = {"q":city,"appid":API_KEY,"units":"metric"}
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+  
+    
+    if data.get("cod") != 200:
+        print("City not Found!")
+        return
+    else:
+         print(f"\nWeather in {city}:")
+         print(f"Temperature: {data['main']['temp']}°C ")
+         print(f"Condition: {data['weather'][0]['description'].title()}")
+         print(f"Humidity: {data['main']['humidity']}%")
+         print(f"Wind Speed: {data['wind']['speed']} m/s")
+    
+city = input("Enter city name: ")
+get_weather(city)    
